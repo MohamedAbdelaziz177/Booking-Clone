e34d05cb8e4c628417e8252387408ecc84e44f53
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-06-27 06:33:03 +0300

    Persistance completed

--- BookingClone.Infrastructure/Persistance/AppDbContext.cs
+++ BookingClone.Infrastructure/Persistance/AppDbContext.cs
@@ -1,8 +1,37 @@
 ﻿
+using BookingClone.Domain.Entities;
+using BookingClone.Infrastructure.Persistance.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookingClone.Infrastructure.Persistance;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<User>
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        new FeedbackConfiguration().Configure(builder.Entity<FeedBack>());
+        new HotelConfiguration().Configure(builder.Entity<Hotel>());
+        new ReservationConfiguration().Configure(builder.Entity<Reservation>());
+        new RoomConfiguration().Configure(builder.Entity<Room>());
+        new RoomImageConfiguration().Configure(builder.Entity<RoomImage>());
+        new UserConfiguration().Configure(builder.Entity<User>());
+
+    }
+
+    public DbSet<Hotel> hotels { get; set; }
+
+    public DbSet<Room> rooms { get; set; }
+
+    public DbSet<FeedBack> feedBacks { get; set; }
+
+    public DbSet<RoomImage> roomsImage { get; set; }
+
+    public DbSet<Reservation> reservations { get; set; }
+    
 }
