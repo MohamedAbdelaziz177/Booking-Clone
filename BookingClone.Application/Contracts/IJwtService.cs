27a63c7b9a53283d7f3361e405da02b4d31d3f23
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-06-28 06:22:49 +0300

    Adding jwt util to the systems

---
+++ BookingClone.Application/Contracts/IJwtService.cs
@@ -0,0 +1,16 @@
+﻿
+using BookingClone.Application.Features.Auth.Responses;
+using BookingClone.Domain.Entities;
+
+namespace BookingClone.Application.Contracts;
+
+public interface IJwtService
+{
+    string GenerateAccessToken(User user);
+
+    string GenerateRefreshToken(string refreshToken);
+
+    TokenResponseDto GetTokens(User user);
+
+    bool ValidateRefreshToken(string refreshToken);
+}
---
+++ BookingClone.Infrastructure/Services/JwtService.cs
@@ -0,0 +1,33 @@
+﻿using BookingClone.Application.Contracts;
+using BookingClone.Application.Features.Auth.Responses;
+using BookingClone.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingClone.Infrastructure.Services;
+
+public class JwtService : IJwtService
+{
+    public string GenerateAccessToken(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GenerateRefreshToken(string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TokenResponseDto GetTokens(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ValidateRefreshToken(string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+}
