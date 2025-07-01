b38b9984a41ab88c244c38f4e9e665fe7ee4a1a7
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-07-01 04:23:23 +0300

    Centeralizing system constants

---
+++ BookingClone.Application/Common/MagicValues.cs
@@ -0,0 +1,14 @@
+﻿
+namespace BookingClone.Application.Common;
+
+public static class MagicValues
+{
+    public static readonly int MIN_OTP_VAL = 10000;
+    public static readonly int MAX_OTP_VAL = 100000;
+    public static readonly int OTP_LIFE_TIME_MINS = 5;
+
+    public static readonly int REFRESH_TOKEN_LIFE_TIME_DAYS = 15;
+    public static readonly int ACCESS_TOKEN_LIFE_TIME_MINS = 15;
+
+    public static readonly int OTP_EXPIRY_MINS = 5;
+}
