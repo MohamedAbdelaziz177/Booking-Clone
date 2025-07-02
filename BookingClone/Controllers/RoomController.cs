4e5e2ae34625fab2f01e9b9f5066ca4cdb078f9d
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-07-02 03:16:50 +0300

    Adding Hotel And Room controllers with basic crud operations

---
+++ BookingClone/Controllers/RoomController.cs
@@ -0,0 +1,75 @@
+﻿using BookingClone.Application.Common;
+using BookingClone.Application.Features.Hotel.Commands;
+using BookingClone.Application.Features.Hotel.Queries;
+using BookingClone.Application.Features.Room.Commands;
+using BookingClone.Application.Features.Room.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingClone.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public RoomController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet("{Id:int}")]
+        public async Task<IActionResult> GetRoomById(int Id)
+        {
+            var Query = new GetRoomByIdQuery() { Id = Id };
+            var res = await mediator.Send(Query);
+
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRooms(int PageIdx = 0,
+            string SortField = "Id",
+            string SortDir = "Desc"
+            )
+        {
+            var Query = new GetRoomPageQuery()
+            {
+                PageIdx = PageIdx,
+                PageSize = MagicValues.PAGE_SIZE,
+                SortField = SortField,
+                SortType = Enum.Parse<SortType>(SortDir)
+            };
+
+            var res = await mediator.Send(Query);
+
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRoom(CreateRoomCommand createRoomCommand)
+        {
+            var res = await mediator.Send(createRoomCommand);
+            return Created();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateRoom(UpdateRoomCommand updateRoomCommand)
+        {
+            var res = await mediator.Send(updateRoomCommand);
+            return CreatedAtAction(nameof(GetRoomById), new { id = res.Data!.Id }, res);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteRoom(int id)
+        {
+            var cmd = new DeleteRoomCommand() { Id = id };
+            var res = await mediator.Send(cmd);
+
+            return Ok(res);
+        }
+    }
+}
+ 
