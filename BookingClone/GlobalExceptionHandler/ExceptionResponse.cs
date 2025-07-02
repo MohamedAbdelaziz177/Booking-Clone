29bb6da984ed6b30b81bb74145954ee88733fb01
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-07-02 03:15:49 +0300

    Adding the global exception middleware

---
+++ BookingClone/GlobalExceptionHandler/ExceptionResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace BookingClone.Api.GlobalExceptionHandler
+{
+    public class ExceptionResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
