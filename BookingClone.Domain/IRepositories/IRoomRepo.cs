8e67c5d6ae5ef53d110359a77cf25802c7e4fa08
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-07-03 05:29:14 +0300

    Extending Room repo with methods checking room availability befor booking

--- BookingClone.Domain/IRepositories/IRoomRepo.cs
+++ BookingClone.Domain/IRepositories/IRoomRepo.cs
@@ -4,4 +4,5 @@ using BookingClone.Domain.Entities;
 namespace BookingClone.Domain.IRepositories;
 public interface IRoomRepo : IGenericRepo<Room>
 {
+    Task<bool> CheckAvailableBetweenAsync(int roomId, DateTime start, DateTime end);
 }
--- BookingClone.Infrastructure/Persistance/Repositories/RoomRepo.cs
+++ BookingClone.Infrastructure/Persistance/Repositories/RoomRepo.cs
@@ -1,6 +1,7 @@
 ﻿using BookingClone.Domain.Entities;
 using BookingClone.Domain.IRepositories;
 using BookingClone.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookingClone.Infrastructure.Persistance.Repositories;
 public class RoomRepo : GenericRepo<Room>, IRoomRepo
@@ -8,4 +9,18 @@ public class RoomRepo : GenericRepo<Room>, IRoomRepo
     public RoomRepo(AppDbContext con) : base(con)
     {
     }
+
+    public async Task<bool> CheckAvailableBetweenAsync(int roomId, DateTime start, DateTime end)
+    {
+        
+        return await con.reservations.AnyAsync(x => x.RoomId == roomId 
+        && ( (x.CheckOutDate > start && x.CheckOutDate < end ) || (x.CheckInDate < end && x.CheckInDate > start )));
+
+    }
+
+
+    //public async Task<List<Room>> GetAvailableBetweenAsync(DateTime start, DateTime end)
+    //{
+    //    
+    //}
 }
