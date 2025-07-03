806e42127e7d1c769f11fb9d05a9f4391deb9ee8
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-07-03 04:19:47 +0300

    Reservation Response Dto

--- BookingClone.Application/Features/Reservation/Responses/ReservationResponseDto.cs
+++ BookingClone.Application/Features/Reservation/Responses/ReservationResponseDto.cs
@@ -1,6 +1,28 @@
 ﻿
+using BookingClone.Application.Features.Room.Responses;
+using BookingClone.Domain.Enums;
+
 namespace BookingClone.Application.Features.Reservation.Responses;
 
 public class ReservationResponseDto
 {
+    public int Id { get; set; }
+
+    public string UserId { get; set; } = string.Empty;
+
+    public DateTime BookingDate { get; set; }
+
+    public DateTime CheckInDate { get; set; }
+
+    public DateTime CheckOutDate { get; set; }
+
+
+    public ReservationStatus ReservationStatus { get; set; }
+
+    public int GetNightsNo() => (CheckOutDate - CheckInDate).Days;
+
+    public decimal GetTotalPrice() => GetNightsNo() * RoomCardResponse.PricePerNight;
+
+    public RoomCardResponse RoomCardResponse { get; set; } = new();
+
 }
