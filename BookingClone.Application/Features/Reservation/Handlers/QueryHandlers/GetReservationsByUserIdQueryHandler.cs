9dfcdc746e49d3cef044291b8f524f47ebf07afb
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-07-06 07:10:08 +0300

    Adding Get my rservations query along side with the handler

---
+++ BookingClone.Application/Features/Reservation/Handlers/QueryHandlers/GetReservationsByUserIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BookingClone.Application.Common;
+using BookingClone.Application.Features.Reservation.Queries;
+using BookingClone.Application.Features.Reservation.Responses;
+using BookingClone.Domain.IRepositories;
+using MediatR;
+using System.Collections.Generic;
+using ReservationEntity = BookingClone.Domain.Entities.Reservation;
+
+namespace BookingClone.Application.Features.Reservation.Handlers.QueryHandlers;
+
+public class GetReservationsByUserIdQueryHandler : IRequestHandler<GetReservationsByUserIdQuery, Result<List<ReservationResponseDto>>>
+{
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+
+    public GetReservationsByUserIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+    }
+    public async Task<Result<List<ReservationResponseDto>>> Handle(GetReservationsByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var reservations = await unitOfWork.ReservationRepo.GetByUserIdAsync(request.UserId);
+        var res = reservations.Select(r => mapper.Map<ReservationResponseDto>(r)).ToList();
+
+        return ResultBuilder<List<ReservationResponseDto>>.CreateSuccessResponse(res);
+    }
+}
---
+++ BookingClone.Application/Features/Reservation/Queries/GetReservationsByUserIdQuery.cs
@@ -0,0 +1,11 @@
+﻿
+using BookingClone.Application.Common;
+using BookingClone.Application.Features.Reservation.Responses;
+using MediatR;
+
+namespace BookingClone.Application.Features.Reservation.Queries;
+
+public class GetReservationsByUserIdQuery : IRequest<Result<List<ReservationResponseDto>>>
+{
+    public string UserId { get; set; } = string.Empty;
+}
