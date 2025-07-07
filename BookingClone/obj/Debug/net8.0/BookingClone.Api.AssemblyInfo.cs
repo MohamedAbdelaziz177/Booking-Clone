e4ce752dc7ff082f9c98da127c8354faa12e4e5f
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-07-07 03:29:54 +0300

    Deleting redundant classes

--- BookingClone/obj/Debug/net8.0/BookingClone.Api.AssemblyInfo.cs
+++ BookingClone/obj/Debug/net8.0/BookingClone.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingClone.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+831330c4108c5981740ea2d560354c7029216990")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43fa8733b5991686780771bc5e1dfce112bc584")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingClone.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingClone.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
